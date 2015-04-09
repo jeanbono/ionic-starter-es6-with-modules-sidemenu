050c9274f0aab0242aafb98f264db8b426ad8a54
Pierre Fraisse <pierre.fraisse@nebulous.fr>
2015-04-09 15:31:45 +0200

    init

---
+++ jssrc/app.js
@@ -0,0 +1,16 @@
+import {onReady} from 'bootstrap';
+import {router} from 'router';
+import {AppCtrl} from 'controllers/AppCtrl';
+import {PlaylistsCtrl} from 'controllers/PlaylistsCtrl';
+import {PlaylistCtrl} from 'controllers/PlaylistCtrl';
+
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+  .config(router)
+  .run(onReady);
+
+angular.module('starter.controllers', [])
+  .controller('AppCtrl', AppCtrl)
+  .controller('PlaylistsCtrl', PlaylistsCtrl)
+  .controller('PlaylistCtrl', PlaylistCtrl);
+
+angular.module('starter.services', []);
---
+++ jssrc/controllers/PlaylistCtrl.js
@@ -0,0 +1,7 @@
+export class PlaylistCtrl {
+  constructor($scope, $stateParams)
+  {
+    this.$inject = ['$scope', '$stateParams'];
+
+  }
+}
